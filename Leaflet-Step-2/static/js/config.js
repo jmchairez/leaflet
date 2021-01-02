b767c4fdbfeffa7c5db722fdddad56898e2091d4
Janette Chairez <ChairezFamily@Janettes-iMac-3.local>
2021-01-02 11:52:44 -0600

    fixing folders

--- Leaflet-Step-2/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoiam1jaGFpcmV6IiwiYSI6ImNraWZkbWlsOTFibmYycnF4OXJtbjJncXkifQ.bW5PuN7GBKf_sXMNZfBaMQ";
---
+++ static/js/logic.js
@@ -0,0 +1,50 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+
+d3.json(queryUrl, function(data) {
+  console.log(data.features);
+
+  var eathquakes = L.geoJSON(data.features, 
+    {
+      onEachFeatures: addPopup
+  })
+
+  createMap(eathquakes);
+});
+
+function addPopup(feature, layer) {
+  return layer.bindPopup(`<h3> ${feature.properties.place} </h3> <hr> <p> ${Date(feature.properties.time)} </p>`);
+}
+
+function createMap(earthquakes) {
+
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  maxZoom: 18,
+  id: "streets-v11",
+  accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  maxZoom: 18,
+  id: "dark-v10",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+
+var overlayMaps = {
+  "Earthquakes": earthquakes
+};
+
+var myMap = L.map("mapid", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [streetmap, earthquakes]
+});
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
+};
